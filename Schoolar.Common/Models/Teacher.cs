d1dd045a10311214f2990723a2d73ac9f5ee2b3f
Eduardo Fong Dubon <eduardofongdubon@hotmail.com>
2020-04-03 09:34:09 -0600

    Cracion en proyecto common de modelos Response, TokenRequest, TokenResponse, modificacion de Teacher Creacion de ApiService con métodos

--- Schoolar.Common/Models/Teacher.cs
+++ Schoolar.Common/Models/Teacher.cs
@@ -1,27 +1,38 @@
 ﻿namespace Schoolar.Common.Models
 {
+    using Newtonsoft.Json;
     using System;
 
     public class Teacher
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
 
+        [JsonProperty("firstName")]
         public string FirstName { get; set; }
 
+        [JsonProperty("lastName")]
         public string LastName { get; set; }
 
+        [JsonProperty("phoneNumber")]
         public string PhoneNumber { get; set; }
 
+        [JsonProperty("enrollment")]
         public string Enrollment { get; set; }
 
+        [JsonProperty("userName")]
         public string UserName { get; set; }
 
+        [JsonProperty("password")]
         public string Password { get; set; }
 
+        [JsonProperty("hireDate")]
         public DateTime HireDate { get; set; }
 
+        [JsonProperty("imageUrl")]
         public string ImageUrl { get; set; }
 
+        [JsonProperty("fullImageUrl")]
         public Uri FullImageUrl { get; set; }
     }
 }
