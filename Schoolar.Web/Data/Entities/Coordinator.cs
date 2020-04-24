ac4edc37500a849dceaaf28cf75e50cfc8f97468
gmaro1313 <57234661+gmaro1313@users.noreply.github.com>
2020-04-24 14:13:06 -0500

    Entidad Coordinador Coordinador View Model

---
+++ Schoolar.Web/Data/Entities/Coordinator.cs
@@ -0,0 +1,23 @@
+﻿namespace Schoolar.Web.Data.Entities
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Coordinator: IEntity  
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Hire date")]
+        public DateTime HireDate { get; set; }
+
+        [Display(Name = "Foto")]
+        public string ImageUrl { get; set; }
+        //cambio https://schoolarweb.azurewebsites.net
+        public string FullImageUrl => string.IsNullOrEmpty(ImageUrl) ? null : $"https://schoolarumad.azurewebsites.net{ImageUrl.Substring(1)}";
+        //public string FullImageUrl => string.IsNullOrEmpty(ImageUrl) ? null : $"https://schoolarweb.azurewebsites.net{ImageUrl.Substring(1)}";
+
+        public User User { get; set; }
+    }
+}
+
---
+++ Schoolar.Web/Models/CoordinatorViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Schoolar.Web.Models
+{
+    using Web.Data.Entities;
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Http;
+    public class CoordinatorViewModel : Coordinator
+    {
+        [Required(ErrorMessage = "{0} es required")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "thi field {0} must have between {2} and {1} characters")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "{0} es required")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "thi field {0} must have between {2} and {1} characters")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        [Display(Name = "Confirma password")]
+        public string PasswordConfirm { get; set; }
+
+        public IFormFile ImageFile { get; set; }
+    }
+}
